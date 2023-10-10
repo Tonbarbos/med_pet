e7f8b267e13400412d708a496dc12f36cb7b0ae6
Wesley Alves Santana <113138601+Wesley-AS@users.noreply.github.com>
2023-10-10 10:39:58 -0300

    modificação dia 10/10
    botão "cadastrar animal" redireciona para a página html de mesmo nome; 
    botão calendário mostra/esconde o calendário; 
    clicar em uma data faz aparecer um formulário em primeiro plano;

--- Projeto MedPet/js/calendário.js
+++ Projeto MedPet/js/calendário.js
@@ -1,13 +1,17 @@
-function criarEvento(info){
-  
-}
+var botaoCalen = document.getElementById("calendario");
+botaoCalen.addEventListener("click", function (){
+  botaoCalen.classList.toggle("ativo");
+  document.getElementById("calendar").classList.toggle("none");
+
+});
+document.getElementById("cadastrarAnimal").addEventListener("click", function(){
+  window.location.href = "cadastro_bicho.html";
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
     selectable : true,
-    selectAllow: function(selectInfo){
-      selectInfo.start=info.dateStr;
-    },
     headerToolbar:{
       center: 'eventButton'
     },
@@ -28,16 +32,24 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     },
     dateClick : function(info){
-      var titulo;
-      do{
-        titulo = prompt('titulo:');
-      }while(titulo=='');
-      if(titulo!=null){
-        calendar.addEvent({
-          title: titulo,
-          start: info.dateStr
-        });
-      }
+      let form = document.querySelector("#form");
+      let divForm = document.getElementById("evento");
+      let titulo = document.getElementById("titulo");
+      let data = document.getElementById("data");
+      
+      let botao = document.getElementById("botao");
+      divForm.style.display="block";
+      botao.addEventListener("click", function(){
+        titulo = titulo.value;
+        data = data.value;
+        if(titulo!=null && data!=null){
+          calendar.addEvent({
+            title: titulo,
+            start: data
+          });
+        }
+      });
+      
       
     }
   });

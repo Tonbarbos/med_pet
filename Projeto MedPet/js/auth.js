8ed1787a469577ebefe33d5ded87a5cffc144696
Wesley-AS <wesleyalvesdesantana@gmail.com>
2023-11-07 20:40:10 -0300

    Alterações do dia 07/11/23

--- Projeto MedPet/js/auth.js
+++
@@ -1,4 +0,0 @@
-// a ideia é barrar informações postas nos formuláros que não forem condizentes com o que é pedido
-// dessa forma, evitamos situações indesejadas tanto da parte do cliente quanto do servidor
-// naturalmente, este arquivo deve ser associado a todos os formulários do projeto
-// ps.: ainda no campo das idéias, então não vai ter nada aqui por enquanto

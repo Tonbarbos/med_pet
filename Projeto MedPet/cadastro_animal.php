6ab7bf66516b33ba1f29f62ca18f5dddc5dc2166
Tonbarbos <115956268+Tonbarbos@users.noreply.github.com>
2023-11-20 19:33:26 -0300

    Selecionar tutores/veterinarios cadastrados

--- Projeto MedPet/cadastro_animal.php
+++ Projeto MedPet/cadastro_animal.php
@@ -42,6 +42,53 @@
                     <input type="text" name="alergias" id="alergias">
                     <label for="motivo">Motivos do Tratamento: </label>
                     <textarea name="descr" id="motivo"></textarea>  
+                    <div class="box">
+                        <label for ="selecaotutores">Selecione o tutor</label>
+                        <select name="selecaoTutores">
+                            <?php
+                                include_once "./conexao.php";
+                            
+                                try {
+                                    $connect = new PDO("mysql:host=$server;dbname=" . $dbname, $user, $passwd);
+                                    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                
+                                  
+                                    $consulta = $connect->query("SELECT tut_id, tut_nome FROM tutores");
+                                    while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                                        echo "<option value='" . $linha['tut_id'] . "'>" . $linha['tut_nome'] . "</option>";
+                                    }
+
+                                    $connect = null; // Fechar a conexão
+                                } catch (PDOException $e) {
+                                    die("Erro na conexão: " . $e->getMessage());
+                                }
+                            ?>
+                        </select>
+                    </div>
+                            
+                    <div class="box">
+                        <label for = "selecaoVeterinarios">Selecione o veterinário</label>
+                        <select name="selecaoVeterinarios">
+                            <?php
+                                include_once "./conexao.php";
+                                try {
+                                    
+
+                                    $connect = new PDO("mysql:host=$server;dbname=" . $dbname, $user, $passwd);
+                                    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                
+                                    $consulta = $connect->query("SELECT vet_id, vet_nome FROM veterinarios");
+                                    while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                                        echo "<option value='" . $linha['vet_id'] . "'>" . $linha['vet_nome'] . "</option>";
+                                    }
+                                
+                                    $connect = null; // Fechar a conexão
+                                } catch (PDOException $e) {
+                                    die("Erro na conexão: " . $e->getMessage());
+                                }
+                            ?>
+                        </select>
+                    </div>    
                     
                     <div>Para incluir as fotos TESTE</div>
                     <form action="./cadastro_animal.php" method="post" enctype="multipart/form-data">

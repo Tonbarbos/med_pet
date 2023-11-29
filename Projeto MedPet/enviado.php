0769488a64ae1839df6609bf610774e179911b19
Tonbarbos <115956268+Tonbarbos@users.noreply.github.com>
2023-11-29 14:21:38 -0300

    enviado.php

---
+++ Projeto MedPet/enviado.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css\enviado.css">
+</head>
+<body>
+    <div class="box">
+    <div class="slide-content">
+    <div class="content">
+      
+    <div class="user-comment"><p class="message">Dados inseridos com sucesso</p>
+    </div>
+    <div class="comment-action">
+        <button onclick="history.go(-2);">Retornar</button>
+        <button onclick="history.go(-1);">Inserir mais</button>
+    </div>
+    </div>
+    </div>
+    </div>
+</body>
+</html>

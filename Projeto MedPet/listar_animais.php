010ea7e46177e5d966dd3a856dd57996b8bb0ee7
Tonbarbos <115956268+Tonbarbos@users.noreply.github.com>
2023-11-30 14:06:44 -0300

    adicionando as fotos

--- Projeto MedPet/listar_animais.php
+++ Projeto MedPet/listar_animais.php
@@ -1,11 +1,14 @@
 <?php
 include_once "conexao.php";
 if($_SESSION['tut']){
-    $select="SELECT anim_id as id, tutores_tut_id as idTutor, veterinarios_vet_id as idVet, anim_nome as nome from animais 
-    where tutores_tut_id = ". $_SESSION['id_tut'];
+    $select = "SELECT anim_id as id, tutores_tut_id as idTutor, veterinarios_vet_id as idVet, anim_nome as nome, arquivo.arquivo as arquivo FROM animais
+    LEFT JOIN arquivo ON animais.anim_id = arquivo.animais_anim_id
+    WHERE tutores_tut_id = " . $_SESSION['id_tut'];
 }else if($_SESSION['vet']){
-    $select="SELECT anim_id as id, tutores_tut_id as idTutor, veterinarios_vet_id as idVet, anim_nome as nome from animais 
-    where veterinarios_vet_id = ". $_SESSION['id_vet'];
+    $select = "SELECT animais.anim_id as id, animais.tutores_tut_id as idTutor,animais.veterinarios_vet_id as idVet,animais.anim_nome as nome, arquivo.arquivo as arquivo FROM animais 
+    LEFT JOIN arquivo ON animais.anim_id = arquivo.animais_anim_id 
+    WHERE animais.veterinarios_vet_id = " . $_SESSION['id_vet'];
+}
 }
 
 $preparar = $connect->prepare($select);

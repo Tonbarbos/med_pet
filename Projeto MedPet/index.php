751a4baebdbe2c5fd5ad986e7961ff2014ed3048
Zyendie <147444846+Zyendie@users.noreply.github.com>
2023-11-29 22:23:59 -0300

    Update index.php
    tirei os botões inúteis que não levavam para lugar nenhum

--- Projeto MedPet/index.php
+++ Projeto MedPet/index.php
@@ -10,17 +10,13 @@
     <header>
         <div class="header">
             <div class="logo"><img src="css/imagens/logo.png" alt="Logo"></div>
-                <div class="menu">
-                    <a href="index.php">Sobre</a>
-                    <a href="login.php" class="sticky">Login</a>
-                </div>
         </div>
     </header>
     <section class="sobre">
         <div class="center">
             <div class="sobre">
                 <h1>Um site para acompanhar o<br/>tratamento do seu <span style="color: #178871;">Pet</span>!</h1>
-                <p>O MedPet é um site que tem como objetivo principal auxiliar na gestão de tratamentos de animais em clinicas veterinárias a distância, além disso ele permite que tutores de animais possam ter mais contato com os veterinários de seus animais através do site.</p>
+                <p>O MedPet é um site que tem como objetivo principal auxiliar na gestão de tratamentos de animais em clinicas veterinárias a distância, além disso ele permite que tutores de animais possam ter mais contato com os veterinários de seus animais.</p>
             <div class="botao">
                 <a href="login.php"><span>Conhecer!</span></a>
             </div>

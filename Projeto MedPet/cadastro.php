86219785fcf7ddde20de9152d282601a17449a2a
Wesley-AS <wesleyalvesdesantana@gmail.com>
2023-12-01 01:28:14 -0300

    Removendo campos inutilizados

--- Projeto MedPet/cadastro.php
+++ Projeto MedPet/cadastro.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="css/registro.css">
-    <script src="js/auth.js" defer></script>
 </head>
 <body class="bgcolor">
 

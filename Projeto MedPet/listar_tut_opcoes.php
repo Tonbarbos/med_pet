4ea6e836cca625cef031604f5f75ba87dd36c04b
Tonbarbos <115956268+Tonbarbos@users.noreply.github.com>
2023-12-03 19:02:01 -0300

    Exibir apenas o tutor da sessão

--- Projeto MedPet/listar_tut_opcoes.php
+++ Projeto MedPet/listar_tut_opcoes.php
@@ -1,15 +1,20 @@
 <?php
 include_once "conexao.php";
-$select = "SELECT tut_id, tut_nome FROM tutores";  
+
+$id_tut_sessao = $_SESSION['id_tut'];
+
+$select = "SELECT tut_id, tut_nome FROM tutores WHERE tut_id = :id_tut_sessao";
 $preparar = $connect->prepare($select);
-try{
-    $preparar->execute(); 
-}catch(PDOException $e){
+
+try {
+    $preparar->execute(array(':id_tut_sessao' => $id_tut_sessao));
+    $array = $preparar->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
     echo "Erro ao tentar buscar: " . $e;
 }
-$array = $preparar->fetchall(PDO::FETCH_ASSOC);
-foreach($array as $chave => $valor) {
+
+foreach ($array as $chave => $valor) {
     extract($valor);
-    echo "<option value='" . $tut_id. "''name='tut'>" . $tut_nome . "</option>";
+    echo "<option value='" . $tut_id . "' name='tut'>" . $tut_nome . "</option>";
 }
 ?>

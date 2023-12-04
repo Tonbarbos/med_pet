ca2a38bff0dbe325cae5a9497d85464b19c16490
Wesley Alves Santana <113138601+Wesley-AS@users.noreply.github.com>
2023-12-04 09:14:53 -0300

    Update listar_tut_opcoes.php

--- Projeto MedPet/listar_tut_opcoes.php
+++ Projeto MedPet/listar_tut_opcoes.php
@@ -1,13 +1,11 @@
 <?php
+session_start();
 include_once "conexao.php";
-
-$id_tut_sessao = $_SESSION['id_tut'];
-
-$select = "SELECT tut_id, tut_nome FROM tutores WHERE tut_id = :id_tut_sessao";
+$select = "SELECT tut_id, tut_nome FROM tutores WHERE tut_id =".$_SESSION['id_tut'].";";
 $preparar = $connect->prepare($select);
 
 try {
-    $preparar->execute(array(':id_tut_sessao' => $id_tut_sessao));
+    $preparar->execute();
     $array = $preparar->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo "Erro ao tentar buscar: " . $e;
